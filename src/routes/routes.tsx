ae9ba1e14fa954804049abc8de0c46e89c350d20
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-06-08 15:23:17 -0300

    feat: ajuste routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/login";
 
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,

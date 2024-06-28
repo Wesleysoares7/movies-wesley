746a5bb1edfed559f499abe80e1af119794fe87c
Josieljcc <josiel.jcc@hotmail.com>
2024-06-28 19:22:11 -0300

    fix: icon size inconcistency

--- src/components/form-error/index.tsx
+++ src/components/form-error/index.tsx
@@ -7,7 +7,7 @@ type FormErrorProp = {
 const FormError = ({ children }: FormErrorProp) => {
   return (
     <div className="flex gap-1 text-red-600 items-center justify-center">
-      <VscError />
+      <VscError className="flex-shrink-0 h-5 w-5" />
       <p>{children}</p>
     </div>
   );
